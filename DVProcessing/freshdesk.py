f530de8a4120bc0eabf84ff3fe2ab1011d8d27c1
Alison <github@bankofknowledge.net>
2016-03-13 12:23:31 -0500

    basic freshdesk api access

---
+++ DVProcessing/freshdesk.py
@@ -0,0 +1,5 @@
+from freshdesk.api import API 
+from db_passwords import freshdesk_api_key
+a = API('dvprocesssering.freshdesk.com', freshdesk_api_key)
+
+# https://github.com/sjkingo/python-freshdesk
